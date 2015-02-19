37022d09ca8ee088c4ef44a90eb8d46ffff71471
Jimmy Bernljung <jimmy.bernljung@gmail.com>
2015-02-19 14:09:06 +0100

    More comfort

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -6,5 +6,24 @@ window.onload = function () {
         return false;
      }
   });
+  $('#speech').keyup(function(e) {
+    var text = $('#speech').val();
+    if(text.length == 2){
+      switch (text){
+        case "s:":
+          $('#lang option[value=sv]').prop('selected', 'selected');
+          $('#lang option[value=en]').prop('selected', null);
+          $('#speech').val("");
+          break;
+        case "e:":
+          $('#lang option[value=en]').prop('selected','selected');
+          $('#lang option[value=sv]').prop('selected', null);
+          $('#speech').val("");
+          break;
+        default:
+          break;
+      }
+    }
+  });
   $('#flash.alert-success').fadeOut(7000);
 };
