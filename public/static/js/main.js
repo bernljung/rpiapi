0493cc68635ed26235e7cf09d401efc88fc5092f
Jimmy Bernljung <jimmy.bernljung@gmail.com>
2015-02-19 16:37:35 +0100

    Trimming prefix

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -3,42 +3,33 @@ window.onload = function () {
   $('#speech')[0].setSelectionRange(2, 2);
   $('#speech').keydown(function(e) {
     if (e.keyCode == 13) {
-      var text = $('#speech').val();
+      var text = $.trim($('#speech').val());
       var prefix = text.substr(0,2);
 
-      if(prefix === "e:" || prefix == "s:"){
-        $('#speech').val(text.substr(2, text.length));
-      }
-      $(this.form).submit();
-
-      switch($('#lang').val()){
-        case "en":
-          $('#speech').val("e:");
+      switch(prefix) {
+        case "s:":
+          $('#lang').val("sv");
+          $('#speech').val(text.substr(2, text.length));
           break;
-        case "sv":
-          $('#speech').val("s:");
+        case "e:":
+          $('#lang').val("en");
+          $('#speech').val(text.substr(2, text.length));
           break;
         default:
-          break
+          $('#lang').val("sv");
+          break;
+      }
+
+      $(this.form).submit();
+
+      if ($('#lang').val() === "en") {
+        $('#speech').val("e:");
+      } else {
+        $('#speech').val("s:");
       }
       return false;
      }
   });
 
-  $('#speech').keyup(function(e) {
-    var text = $('#speech').val();
-    if(text.length == 2){
-      switch (text){
-        case "s:":
-          $('#lang').prop('value', 'sv');
-          break;
-        case "e:":
-          $('#lang').prop('value', 'en');
-          break;
-        default:
-          break;
-      }
-    }
-  });
   $('#flash.alert-success').fadeOut(7000);
 };
