30bea3c7a9c58fb41dbadc0f5c09d52014b11265
Jimmy Bernljung <jimmy.bernljung@gmail.com>
2015-02-19 15:32:55 +0100

    prefixed textarea

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,24 +1,39 @@
 window.onload = function () {
   $('#speech').focus();
+  $('#speech')[0].setSelectionRange(2, 2);
   $('#speech').keydown(function(e) {
     if (e.keyCode == 13) {
-        $(this.form).submit()
-        return false;
+      var text = $('#speech').val();
+      var prefix = text.substr(0,2);
+
+      if(prefix === "e:" || prefix == "s:"){
+        $('#speech').val(text.substr(2, text.length));
+      }
+      $(this.form).submit();
+
+      switch($('#lang').val()){
+        case "en":
+          $('#speech').val("e:");
+          break;
+        case "sv":
+          $('#speech').val("s:");
+          break;
+        default:
+          break
+      }
+      return false;
      }
   });
+
   $('#speech').keyup(function(e) {
     var text = $('#speech').val();
     if(text.length == 2){
       switch (text){
         case "s:":
-          $('#lang option[value=sv]').prop('selected', 'selected');
-          $('#lang option[value=en]').prop('selected', null);
-          $('#speech').val("");
+          $('#lang').prop('value', 'sv');
           break;
         case "e:":
-          $('#lang option[value=en]').prop('selected','selected');
-          $('#lang option[value=sv]').prop('selected', null);
-          $('#speech').val("");
+          $('#lang').prop('value', 'en');
           break;
         default:
           break;
