a7504d1a720514bbdb5805aafc0328912ad5476c
Jimmy Bernljung <jimmy.bernljung@gmail.com>
2015-02-23 09:45:36 +0100

    Address for ws

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -3,7 +3,7 @@ $(function() {
   var reconnectIntervalId;
 
   var connect = function(){
-    conn = new WebSocket("ws://" + window.location.hostname + ":8080/ws");
+    conn = new WebSocket("ws://" + window.location.host + "/ws");
     return conn.readyState === 1;
   }
 
